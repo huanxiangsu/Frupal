31fc913345aee0def6873e6d2d3d8fc515f9c978
huanxiangsu <44109791+huanxiangsu@users.noreply.github.com>
2018-11-01 01:16:27 -0700

    moved some stuff here
    just move some stuff from game_map.js to here.

---
+++ JS/game.js
@@ -0,0 +1,24 @@
+var hero_energy = 100;
+var xpos = 0;
+var ypos = 0;
+
+document.getElementById("currentlocation").value=get_hero_position();
+document.getElementById("energy").value=get_energy();
+
+// get the coordinates of the position the hero is at
+function get_hero_position(){
+	return xpos + "," + ypos;
+}
+// update location of the hero
+function updateloc(){
+	document.getElementById("currentlocation").value=get_hero_position();	
+}
+// get the amount of energy the hero has left
+function get_energy(){
+	return hero_energy;
+}
+// decrement energy by one
+function update_energy(){
+	document.getElementById("energy").value = get_energy() - 1;
+	hero_energy--;
+}
