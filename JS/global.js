6b7ff1a47344564f0a335efb3e045e6b0b27b575
huanxiangsu <44109791+huanxiangsu@users.noreply.github.com>
2018-11-01 21:50:56 -0700

    Update global.js

--- JS/global.js
+++ JS/global.js
@@ -1,34 +1,47 @@
 //File where global variables will are located
 
-var map = [0,0];  // two dimensional array to store each map cell
+/* ************************ map file information **************************/
+var title = "";
+var mapSize = 0;
 
 //Elements for the hero game
 var hero = {
-  title: "Test Map",
-  map_size: 25,
   row_coordinate: 0,
   column_coordinate: 0,
-  energy: 100,
-  whiffles: 1000,
-  items: [
-    "Axe", 
-    "Axe", 
-    "Shears", 
-    "Pretty Rock"],
-  known_cells: [
-    [12,12,1,1,"None"],
-    [13,12,0,1,"Tree"],
-    [14,12,0,2,"None"]],
+  energy: 0,   // default
+  whiffles: 0,  // default
+  items: [],
 }
 
-var cellSize = 40;  // each square's size measured in px. (can be any value)
-var hero_left_pos = 0; // used to move hero within the graphic map
+var map;  // two dimensional array to store each map cell
+// when parsing script performed successfully, it can use as:
+// example: map[3][5].row  map[3][5].col  map[3][5].visibility  map[3][5].terrain  map[3][5].obstacle
+// each element in this two dimensional array is a class of map_cell that declared below.
+
+class map_cell {  // for each map cell's property
+    constructor(x, y) {
+        this.row = x;
+        this.col = y;
+        this.visibility = 0;
+        this.terrain = 0;
+        this.obstacle = "None";
+    }
+}
+/* ************************* End map file information ******************************/
+
+
+
+/*************************** graphic map information ******************************/
+var cellSize = 40;  // each square's actual size measured in px. (can be any value)
+var hero_left_pos = 0; // the graphic hero's position measured in px.
 var hero_top_pos = 0;  // same above
 
 var graphic_map = document.getElementById("map");
 var startPage = document.getElementById("startPage");
 var gamePage = document.getElementById("game");
 var graphic_hero = document.getElementById("hero");
+/*************************** End graphic map information ***************************/
 
-//The array that will store the information above
+
+// for state-preserving file 
 var saved_map_info = [];
