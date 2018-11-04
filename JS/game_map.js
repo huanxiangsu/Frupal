1c32b1c31809975d50593093e8b26595f3d0bcef
huanxiangsu <44109791+huanxiangsu@users.noreply.github.com>
2018-11-03 19:02:10 -0700

    For graphic map only
    Now setupMap() can setup a cell with appropriate terrain and obstacle or item, and everything else will be setup properly based on the value from the map file.
    ***NOTE: This file should only contain scripts that manipulate the property of the graphic map. any unrelated codes should place in another file.

--- JS/game_map.js
+++ JS/game_map.js
@@ -1,19 +1,23 @@
-var cellSize = 40;  // each square's size measured in px. (can be any value)
-var hero_left_pos = 0; // used to move hero within the graphic map
-var hero_top_pos = 0;  // same above
+/*
+  This file should only contain codes for graphic map manipulation
+*/
 
-var graphic_map = document.getElementById("map");
-var startPage = document.getElementById("startPage");
-var gamePage = document.getElementById("game");
-var graphic_hero = document.getElementById("hero");
-
+/*
+  these 4 lines should call another function to handle the actual hero movement instead of graphic movement function,
+  which is the program from user story of hero's movement.
+  these four lines could move to global.js or a file to handle movement script.
+*/
 document.getElementById("moveUp").addEventListener("click", move_hero_up_graph);
 document.getElementById("moveDown").addEventListener("click", move_hero_down_graph);
 document.getElementById("moveLeft").addEventListener("click", move_hero_left_graph);
 document.getElementById("moveRight").addEventListener("click", move_hero_right_graph);
+// 
 
 
-// to setup a basic map, all cells will set to default meadow terrain.
+/*
+  The main function to setup a basic graphic map
+  call this function when enter a new game or continue game
+*/
 function setupMap(){
     setUpMapSize();  // setup the size of the map frame
     setupHero();  // set the hero size to fit into a square and put it in (0,0).
@@ -21,16 +25,22 @@ function setupMap(){
     var l_pos = 0;
     var u_pos = 0;
     var row = 0;
-    var id = "";  // each square will have a unique id as a matrix, like 0-0, 0-1
+    
     for(i = 0; i < mapSize; i++){
         u_pos = i * cellSize;
         row = mapSize - (i+1);  // row from (mapSize-1) to 0
         for(j = 0; j < mapSize; j++){
             l_pos = j * cellSize;
-            id = row + "-" + j;       
-            createMeadowCell(l_pos, u_pos, id);  // default will set all cells to meadow
+            createTerrainCell(l_pos, u_pos, row, j, map[row][j].terrain);
+            create_item_in_cell(l_pos, u_pos, row, j, map[row][j].obstacle);
+            //create_item_in_cell(l_pos, u_pos, row, j, "Powerbar");
+            if(map[row][j].visibility == 1){
+                display_one_block(row, j);
+            }
         }
     }
+    display_one_block_around(hero.row_coordinate, hero.column_coordinate);
+    //display_all_blocks();  // for testing only
 }
 
 
@@ -45,32 +55,65 @@ function setUpMapSize(){
 function setupHero(){
     graphic_hero.style.height = cellSize + 'px';
     graphic_hero.style.width = cellSize + 'px';
-    graphic_hero.style.backgroundSize = cellSize + 'px';
+    hero_top_pos = mapSize * cellSize - cellSize * (hero.row_coordinate + 1);
+    hero_left_pos = hero.column_coordinate * cellSize;
+    graphic_hero.style.left = hero_left_pos + 'px';
+    graphic_hero.style.top = hero_top_pos + 'px';
+}
+
+function createTerrainCell(leftPosition, upPosition, row, col, terrain){
+    var x = document.createElement("DIV");
+    var ID = row + "-" + col;
+    if(terrain == 0)  // meadow
+        x.setAttribute("class", "default-cell meadow un-viewable");
+    else if(terrain == 1)  // forest
+        x.setAttribute("class", "default-cell forest un-viewable");
+    else if(terrain == 2)  // river
+        x.setAttribute("class", "default-cell water un-viewable");
+    else if(terrain == 3)  // wall
+        x.setAttribute("class", "default-cell wall un-viewable");
+    else if(terrain == 4)  // bog
+        x.setAttribute("class", "default-cell bog un-viewable");
+    else if(terrain == 5)  // swamp
+        x.setAttribute("class", "default-cell swamp un-viewable");
+    else  // for any number other than 0-5, set to default meadow
+        x.setAttribute("class", "default-cell meadow un-viewable");
     
-    hero_top_pos = mapSize * cellSize - cellSize;
-    hero_left_pos = 0;
-    graphic_hero.style.left = hero_left_pos;
-    graphic_hero.style.top = mapSize * cellSize - cellSize + 'px';
+    x.setAttribute("ID", ID);
+    x.setAttribute("style", "left:" + leftPosition + "px; top:" + upPosition + "px; height:" + cellSize + "px; width:" + cellSize + "px;");
+    graphic_map.appendChild(x);
 }
 
 
-// Function to create a cell (a div element) with meadow terrain
-// id: an id is a matrix coordinate, like "0-0", "1-5".
-function createMeadowCell(leftPosition, upPosition, ID){
-    var x = document.createElement("DIV");
-    x.setAttribute("class", "default-cell meadow un-viewable");
-    x.setAttribute("ID", ID);
-    x.setAttribute("style", "left:"+leftPosition+"px; top:"+upPosition+"px; height:" + cellSize + "px; width:" + cellSize + "px; background-size:" + cellSize + "px");
-    //x.classList.remove("un-viewable");
-    graphic_map.appendChild(x);
+// to add obstacle or item in the cell
+function create_item_in_cell(leftPosition, upPosition, row, col, cell_item){
+    if(cell_item != "None"){  
+        var ID = row + "--" + col;
+        var x = document.createElement("DIV");
+        x.setAttribute("class", "default-item " + cell_item);
+        x.setAttribute("ID", ID);
+        x.setAttribute("style", "left:"+leftPosition+"px; top:"+upPosition+"px; height:" + cellSize + "px; width:" + cellSize + "px;");
+        graphic_map.appendChild(x);
+    }
+}
+
+// to remove obstacle or item from the cell
+function remove_item_in_cell(row, col){
+    var ID = row + '--' + col;
+    var a_item = document.getElementById(ID);
+    a_item.parentNode.removeChild(a_item);
 }
 
 
 // remove all div elements within the map when exit.
 function removeAllCells(){
     var cells = document.getElementsByClassName("default-cell");
+    var items = document.getElementsByClassName("default-item");
     while(cells.length > 0){
         cells[0].parentNode.removeChild(cells[0]);
+    }
+    while(items.length > 0){
+        items[0].parentNode.removeChild(items[0]);
     }
 }
 
@@ -83,7 +126,8 @@ function display_one_block(row, column){
 }
 
 
-// to display one block ahead when having one bincolur
+// to display one block around the hero when having one bincolur
+// also update the visibility value of those viewable blocks.
 function display_one_block_around(row, column){
     var coordinate = "";
     var i, j;
@@ -96,12 +140,13 @@ function display_one_block_around(row, column){
                 continue;
             }
             coordinate = i + "-" + j;
+            map[i][j].visibility = 1;  // update map file
             document.getElementById(coordinate).classList.remove("un-viewable");
         }
     }
 }
 
-// to display two block ahead when having a pair of bincolur
+// to display two block ahead when having a pair of bincolurs
 function display_two_block_around(row, column){
     var coordinate = "";
     var i, j;
@@ -114,20 +159,37 @@ function display_two_block_around(row, column){
                 continue;
             }
             coordinate = i + "-" + j;
+            map[i][j].visibility = 1;  // update map file
             document.getElementById(coordinate).classList.remove("un-viewable");
         }
     }
 }
 
+// to display all blocks - for testing only
+function display_all_blocks(){
+    var cells = document.getElementsByClassName("default-cell");
+    var i = 0;
+    while(i < cells.length){
+        cells[i].classList.remove("un-viewable");
+        ++i;
+    }
+}
 
 
 // function to move the hero up one square in the graphic map
+/*  
+  the actual movement function should call these four functions when hero's coordinate successfully changed
+  instead of writing any codes that are unrelated to graphic map within these four functions.
+  these four functions are just a small part of the actual movement scripts that manipulate 
+  the hero's real coordinate that from the map file.
+*/
 function move_hero_up_graph(){
     if(hero_top_pos > 0)
         hero_top_pos -= cellSize;
     else   /* hero will cross to another side */
         hero_top_pos = mapSize * cellSize - cellSize;
-    graphic_hero.style.top = hero_top_pos + 'px';
+    $("#hero").animate({top : hero_top_pos + 'px'}, 200);
+    //graphic_hero.style.top = hero_top_pos + 'px';
 }
 
 function move_hero_down_graph(){
@@ -135,7 +197,8 @@ function move_hero_down_graph(){
         hero_top_pos += cellSize;
     else
         hero_top_pos = 0;
-    graphic_hero.style.top = hero_top_pos + 'px';
+    $("#hero").animate({top : hero_top_pos + 'px'}, 200);
+    //graphic_hero.style.top = hero_top_pos + 'px';
 }
 
 function move_hero_left_graph(){
@@ -143,7 +206,8 @@ function move_hero_left_graph(){
         hero_left_pos -= cellSize;
     else
         hero_left_pos = mapSize * cellSize - cellSize;
-    graphic_hero.style.left = hero_left_pos + 'px';
+    $("#hero").animate({left : hero_left_pos + 'px'}, 200);
+    //graphic_hero.style.left = hero_left_pos + 'px';
 }
 
 function move_hero_right_graph(){
@@ -151,5 +215,27 @@ function move_hero_right_graph(){
         hero_left_pos += cellSize;
     else
         hero_left_pos = 0;
-    graphic_hero.style.left = hero_left_pos + 'px';
+    $("#hero").animate({left : hero_left_pos + 'px'}, 200);
+    //graphic_hero.style.left = hero_left_pos + 'px';
 }
+
+
+// to support keyboard event for moving the hero.
+// ↑ or w for up, ↓ or s for down, ← or a for left, → or d for right
+$("#map").keyup(function(){
+    //console.log(event);
+    var x = event.which;
+    if (x == 37 || x == 65) {  //left: ← or a
+        move_hero_left_graph();
+    }
+    else if (x == 38 || x == 87) {  // up: ↑ or w
+        move_hero_up_graph();
+    }
+    else if (x == 39 || x == 68) {  // right: → or d
+        move_hero_right_graph();
+    }
+    else if (x == 40 || x == 83) {  // down: ↓ or s
+        move_hero_down_graph();
+    }
+    else{}
+});
