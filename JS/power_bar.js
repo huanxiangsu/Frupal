ad568fa7a814128da3f2010dcc55ae0c2bb202da
huanxiangsu <44109791+huanxiangsu@users.noreply.github.com>
2018-11-25 00:53:56 -0800

    Update power_bar.js

--- JS/power_bar.js
+++ JS/power_bar.js
@@ -1,13 +1,31 @@
 // this function creates the power bar on the map at a random x,y location, is called once upon games start
 function create_power_bar(){
-	powerbar.x = Math.floor(Math.random()*(mapSize));
-	powerbar.y = Math.floor(Math.random()*(mapSize));
-	map[powerbar.x][powerbar.y].obstacle = "Powerbar";	
+    var row, column;
+    var i = 0;
+    var added = 0;
+    while(i < mapSize*mapSize){
+        row = Math.floor(Math.random()*(mapSize));
+        column = Math.floor(Math.random()*(mapSize));
+
+        // if this location contains other item already, loop again. default value for obstacle is "None"
+        if(map[row][column].obstacle != "None"){
+            ++i;
+            continue;
+        }
+        else{
+            map[row][column].obstacle = "Powerbar";
+	    ++i;
+	    ++added;
+	    if(added == 5) // add up to 5 powerbar
+              break;
+        }
+    }	
 }
+
 // function that removes the power bar from the map, this function is called once a power bar is purchased
 function delete_power_bar(){
 	remove_item_in_cell(hero.row_coordinate, hero.column_coordinate);
-	map[powerbar.x][powerbar.y].obstacle = "None";
+	map[hero.row_coordinate][hero.column_coordinate].obstacle = "None";
 	powerbar.x = null;
 	powerbar.y = null;
 }
